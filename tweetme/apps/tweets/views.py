50accc01724419941b130274ee73a65e59274673
kx <jiwen033@gmail.com>
2021-04-03 22:45:04 +0200

    tweet list view api

--- tweetme/apps/tweets/views.py
+++ tweetme/apps/tweets/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from tweetme.apps.tweets.models import Tweet
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     try:
-        print (tweet_id)
         obj = Tweet.objects.get(id=tweet_id)
     except:
         raise Http404
@@ -15,11 +14,12 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
         request, "tweet_detail.html",
         context={}, status=200)
 
-
 def tweet_list_view(request, *args, **kwargs):
-    try:
-        obj = Tweet.objects.get()
-    except:
-        raise Http404
+    tweets = Tweet.objects.all()
+    tweet_list = [{'id': tweet.id, 'content': tweet.content} for tweet in tweets]
+    data = {
+        "reponse": tweet_list
+    }
+    return JsonResponse(data)
 
-    return HttpResponse(obj.id)
+    
