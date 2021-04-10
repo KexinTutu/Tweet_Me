7fb8b50b19176af7ca43a71490d8bf3373c59478
kx <jiwen033@gmail.com>
2021-04-10 23:30:31 +0200

    add user as tweet foreigh key

--- tweetme/apps/tweets/models.py
+++ tweetme/apps/tweets/models.py
@@ -1,8 +1,14 @@
 import random
+
+from django.conf import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL
+
 
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE )
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
